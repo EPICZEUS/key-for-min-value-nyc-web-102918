011c5930818f1e559c1b8d11900850f49179c42d
Sam B <s77broz@aol.com>
2018-10-12 19:26:10 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,9 @@ def key_for_min_value(name_hash)
   keyF = nil
   min = 1000000
   name_hash.each do |key, val|
-    keyF = key if val < min
+    if val < min
+      min = val
+      keyF = key
     binding.pry
   end
   keyF
